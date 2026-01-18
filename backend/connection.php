316b4c782e2e1a01398d8d350b45f025519a81ff
Mohamed Tarek <mohamedtarekbadr047@gmail.com>
2026-01-19 01:40:33 +0200

    Edit Connection

--- backend/connection.php
+++ backend/connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$connection= new mysqli("localhost", "root", "", "ushering");
+$connection = new mysqli("sql313.infinityfree.com", "if0_40296491", "cEP6EEQIV9", "if0_40296491_threedosushering");
 
-if($connection->connect_error){
+if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 
 }
